b179c0bbbba07a3f620282404c8f4ee2ef1bed73
popobo <331361996@qq.com>
2023-05-23 00:15:00 +0800

    template project commit

---
+++ 2-1_project_template/User/main.c
@@ -0,0 +1,17 @@
+#include "stm32f10x.h"                  // Device header
+
+int main()
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	GPIO_InitTypeDef GPIO_init_structure;
+	GPIO_init_structure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_init_structure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_init_structure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_init_structure);
+	// GPIO_SetBits(GPIOC, GPIO_Pin_13);
+	GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+	while(1)
+	{
+		
+	}
+}
