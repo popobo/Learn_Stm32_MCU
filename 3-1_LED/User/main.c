d193802591665dbae4c579fb33e0ccccf937c624
popobo <331361996@qq.com>
2023-05-23 10:16:30 +0800

    light a led

--- 3-1_LED/User/main.c
+++ 3-1_LED/User/main.c
@@ -1,17 +1,21 @@
 #include "stm32f10x.h"                  // Device header
+#include "Delay.h"
 
 int main()
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-	GPIO_InitTypeDef GPIO_init_structure;
-	GPIO_init_structure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_init_structure.GPIO_Pin = GPIO_Pin_13;
-	GPIO_init_structure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC, &GPIO_init_structure);
-	// GPIO_SetBits(GPIOC, GPIO_Pin_13);
-	GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-	while(1)
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    GPIO_InitTypeDef GPIO_init_stru;
+    GPIO_init_stru.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_init_stru.GPIO_Pin = GPIO_Pin_0;
+    GPIO_init_stru.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_init_stru);
+	//GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+    GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
+    while(1)
 	{
-		
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
+        Delay_ms(500);
+        GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+        Delay_ms(500);
 	}
 }
