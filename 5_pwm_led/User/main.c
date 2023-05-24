c4eb65dbd0a23c34fb0c854bb92ed30643fb9693
popobo <331361996@qq.com>
2023-05-24 15:03:12 +0800

    initial commit

---
+++ 5_pwm_led/User/main.c
@@ -0,0 +1,36 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "Led.h"
+#include "Key.h"
+#include "Buzzer.h"
+#include "LightSensor.h"
+#include "OLed.h"
+#include "CountSensor.h"
+#include "Encoder.h"
+#include "Timer.h"
+
+static int32_t count;
+
+void Count(void)
+{
+    count++;
+}
+
+void Encoder_Count(int8_t value)
+{
+    count = count + value;
+}
+
+// 不要在中断函数和主函数调用相同的函数或操作同一个硬件,因为硬件不会保存上下文
+int main()
+{   
+    OLED_Init();
+    Timer_Set_Handler(Count);
+    Timer_Init();
+    CountSensor_Init();
+    while(1)
+	{
+        OLED_ShowSignedNum(1, 1, count, 4);
+        OLED_ShowNum(2, 1, Timer_GetCounter(), 5);
+	}
+}
